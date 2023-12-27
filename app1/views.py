7ebb7affbcee998d6c391bec94c1e4f913ee6fd2
Akshay07051994 <akshaygingle18@gmail.com>
2023-12-27 16:53:00 +0530

    app1_view

--- app1/views.py
+++ app1/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .models import Student
+from .serializers import StudentSerializer
 # Create your views here.
+
+
+class StudentAPI(generics.ListCreateAPIView):
+    serializer_class = StudentSerializer
+    queryset = Student.objects.all()
+
+
+class StudentDetailAPI(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = StudentSerializer
+    queryset = Student.objects.all()
